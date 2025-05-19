933a60f21f48c247a0719e67f3cd0109bc354127
Shaik Tousifur Rehaman <157903295+shaikTousif1308@users.noreply.github.com>
2025-05-19 08:29:09 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,11 @@
+// Initialize Typed.js
+document.addEventListener('DOMContentLoaded', function() {
+    var typed = new Typed(".text", {
+        strings: ["Frontend Developer", "Web Designer", "Full Stack Developer"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+});
+
